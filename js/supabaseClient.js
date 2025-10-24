fea72203fc2bd5fe3309747db72145f27b148279
FyndBo <fyndbo.m@gmail.com>
2025-10-24 16:08:47 +0200

    supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,7 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+
+export const supabase = createClient(
+  "https://ymqwoiuhummwrtzfhbgm.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltcXdvaXVodW1td3J0emZoYmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMDYzOTAsImV4cCI6MjA3Njg4MjM5MH0.9s0iaJm_RGJxZFyPZqQyZvPCeCz60Q6P8EsPxfSO2AI"
+)
 
